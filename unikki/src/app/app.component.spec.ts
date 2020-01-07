56226ef8239d23b8f1d29cf86917ef809c4846e3
tkdalic <himagami126@gmail.com>
2020-01-07 23:37:50 +0900

    modify: app spec testの削除

--- unikki/src/app/app.component.spec.ts
+++ unikki/src/app/app.component.spec.ts
@@ -15,19 +15,4 @@ describe("AppComponent", () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'unikki'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual("unikki");
-  });
-
-  it("should render title", () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector(".content span").textContent).toContain(
-      "unikki app is running!"
-    );
-  });
 });
