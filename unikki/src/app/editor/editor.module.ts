2537425bb8c0fe58afeedc72803349a2aea07741
tkdalic <himagami126@gmail.com>
2020-01-08 17:38:41 +0900

    build: ng g component editor/index

--- unikki/src/app/editor/editor.module.ts
+++ unikki/src/app/editor/editor.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
 import { EditorRoutingModule } from "./editor-routing.module";
+import { IndexComponent } from "./index/index.component";
 
 @NgModule({
-  declarations: [],
+  declarations: [IndexComponent],
   imports: [CommonModule, EditorRoutingModule]
 })
 export class EditorModule {}
