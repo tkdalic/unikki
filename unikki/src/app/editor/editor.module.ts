6654d56c446f0a5e9673ac7de76892a2cd6af2fc
tkdalic <himagami126@gmail.com>
2020-01-08 17:40:51 +0900

    modify: editor module tuimoduleの追加

--- unikki/src/app/editor/editor.module.ts
+++ unikki/src/app/editor/editor.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from "@angular/common";
 
 import { EditorRoutingModule } from "./editor-routing.module";
 import { IndexComponent } from "./index/index.component";
+import { TuiModule } from "ngx-tui-editor";
 
 @NgModule({
   declarations: [IndexComponent],
-  imports: [CommonModule, EditorRoutingModule]
+  imports: [CommonModule, EditorRoutingModule, TuiModule]
 })
 export class EditorModule {}
