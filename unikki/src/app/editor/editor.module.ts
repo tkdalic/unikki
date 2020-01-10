3b72ad50a35114a9e2deab0b9fc9508f84c10e9b
tkdalic <himagami126@gmail.com>
2020-01-10 15:20:49 +0900

    modify: editor module tuiModuleの削除

--- unikki/src/app/editor/editor.module.ts
+++ unikki/src/app/editor/editor.module.ts
@@ -3,11 +3,10 @@ import { CommonModule } from "@angular/common";
 
 import { EditorRoutingModule } from "./editor-routing.module";
 import { IndexComponent } from "./index/index.component";
-import { TuiModule } from "ngx-tui-editor";
 import { MarkdownEditorComponent } from "./markdown-editor/markdown-editor.component";
 
 @NgModule({
   declarations: [IndexComponent, MarkdownEditorComponent],
-  imports: [CommonModule, EditorRoutingModule, TuiModule]
+  imports: [CommonModule, EditorRoutingModule]
 })
 export class EditorModule {}
