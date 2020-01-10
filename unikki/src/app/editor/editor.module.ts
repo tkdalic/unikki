e691fe5384ff18aa68ef542c6f1037b48439ba72
tkdalic <himagami126@gmail.com>
2020-01-10 21:51:22 +0900

    build: yarn ng g component editor/todo-list

--- unikki/src/app/editor/editor.module.ts
+++ unikki/src/app/editor/editor.module.ts
@@ -6,9 +6,10 @@ import { EditorRoutingModule } from "./editor-routing.module";
 import { IndexComponent } from "./index/index.component";
 import { MarkdownEditorComponent } from "./markdown-editor/markdown-editor.component";
 import { TodoComponent } from "./todo/todo.component";
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 @NgModule({
-  declarations: [IndexComponent, MarkdownEditorComponent, TodoComponent],
+  declarations: [IndexComponent, MarkdownEditorComponent, TodoComponent, TodoListComponent],
   imports: [CommonModule, EditorRoutingModule, FormsModule]
 })
 export class EditorModule {}
