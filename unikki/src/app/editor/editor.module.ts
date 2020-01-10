167212b602fe6cf8a463fd0333aa3dcccafaf42f
tkdalic <himagami126@gmail.com>
2020-01-10 14:35:51 +0900

    build: yarn ng g component editor/markdown-editor

--- unikki/src/app/editor/editor.module.ts
+++ unikki/src/app/editor/editor.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from "@angular/common";
 import { EditorRoutingModule } from "./editor-routing.module";
 import { IndexComponent } from "./index/index.component";
 import { TuiModule } from "ngx-tui-editor";
+import { MarkdownEditorComponent } from "./markdown-editor/markdown-editor.component";
 
 @NgModule({
-  declarations: [IndexComponent],
+  declarations: [IndexComponent, MarkdownEditorComponent],
   imports: [CommonModule, EditorRoutingModule, TuiModule]
 })
 export class EditorModule {}
---
+++ unikki/src/app/editor/markdown-editor/markdown-editor.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-markdown-editor",
+  templateUrl: "./markdown-editor.component.html",
+  styleUrls: ["./markdown-editor.component.scss"]
+})
+export class MarkdownEditorComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
