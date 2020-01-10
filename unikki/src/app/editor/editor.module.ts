a9d9f741033016c0645cbeac6a27b5e41f4fb08e
tkdalic <himagami126@gmail.com>
2020-01-10 22:48:23 +0900

    refactor: editor module prettier

--- unikki/src/app/editor/editor.module.ts
+++ unikki/src/app/editor/editor.module.ts
@@ -6,10 +6,15 @@ import { EditorRoutingModule } from "./editor-routing.module";
 import { IndexComponent } from "./index/index.component";
 import { MarkdownEditorComponent } from "./markdown-editor/markdown-editor.component";
 import { TodoComponent } from "./todo/todo.component";
-import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoListComponent } from "./todo-list/todo-list.component";
 
 @NgModule({
-  declarations: [IndexComponent, MarkdownEditorComponent, TodoComponent, TodoListComponent],
+  declarations: [
+    IndexComponent,
+    MarkdownEditorComponent,
+    TodoComponent,
+    TodoListComponent
+  ],
   imports: [CommonModule, EditorRoutingModule, FormsModule]
 })
 export class EditorModule {}
