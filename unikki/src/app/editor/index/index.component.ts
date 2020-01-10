2c8ebbbda3db6c260b1d2f5a350f0a2b16ea596b
tkdalic <himagami126@gmail.com>
2020-01-10 14:11:27 +0900

    build: npx -p @storybook/cli sb init

--- unikki/src/app/editor/index/index.component.ts
+++ unikki/src/app/editor/index/index.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./index.component.scss"]
 })
 export class IndexComponent implements OnInit {
+  editorOptions = {
+    previewStyle: "tab"
+  };
   constructor() {}
 
   ngOnInit() {}
