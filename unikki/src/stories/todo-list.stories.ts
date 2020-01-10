aac13fc1b90779b28da0c91e0d58d9bca481f3b8
tkdalic <himagami126@gmail.com>
2020-01-10 22:49:03 +0900

    modify: storybook todo-list todoを表示できるように

--- unikki/src/stories/todo-list.stories.ts
+++ unikki/src/stories/todo-list.stories.ts
@@ -1,11 +1,26 @@
 import { boolean, text, withKnobs } from "@storybook/addon-knobs";
-import { TodoListComponent } from 'src/app/editor/todo-list/todo-list.component';
+import { TodoListComponent } from "src/app/editor/todo-list/todo-list.component";
+import { NgModule } from "@angular/core";
+import { moduleMetadata } from "@storybook/angular";
+import { TodoComponent } from "src/app/editor/todo/todo.component";
 
 export default {
   title: "todo-list",
-  decorators: [withKnobs]
+  decorators: [
+    withKnobs,
+    moduleMetadata({
+      declarations: [TodoComponent]
+    })
+  ]
 };
 
 export const todoList = () => ({
   component: TodoListComponent,
+  props: {
+    tasks: [
+      { text: "hoge 1", check: false },
+      { text: "hoge 2", check: true },
+      { text: "hoge 3", check: false }
+    ]
+  }
 });
