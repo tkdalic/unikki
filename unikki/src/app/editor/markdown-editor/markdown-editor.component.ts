23e263abebeed0bdf5dcf6aa95001c75df8ed82c
tkdalic <himagami126@gmail.com>
2020-01-10 16:03:15 +0900

    modify: markdown-editor tui-editorの表示

--- unikki/src/app/editor/markdown-editor/markdown-editor.component.ts
+++ unikki/src/app/editor/markdown-editor/markdown-editor.component.ts
@@ -1,12 +1,26 @@
-import { Component, OnInit } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  ViewEncapsulation
+} from "@angular/core";
+import Editor from "tui-editor";
 
 @Component({
   selector: "app-markdown-editor",
   templateUrl: "./markdown-editor.component.html",
-  styleUrls: ["./markdown-editor.component.scss"]
+  styleUrls: ["./markdown-editor.component.scss"],
+  encapsulation: ViewEncapsulation.None
 })
 export class MarkdownEditorComponent implements OnInit {
+  @ViewChild("markdownEditor", { static: true }) markdownEditor: ElementRef;
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    new Editor({
+      el: this.markdownEditor.nativeElement,
+      previewStyle: "tab"
+    });
+  }
 }
