94bd43695e3fe31706ff6150df2396beb850d5cd
tkdalic <himagami126@gmail.com>
2020-01-11 20:37:56 +0900

    modify: todo-focus enter押下時、次の要素へ移動

--- unikki/src/app/editor/directive/todo-focus/todo-focus.directive.ts
+++ unikki/src/app/editor/directive/todo-focus/todo-focus.directive.ts
@@ -1,8 +1,32 @@
-import { Directive } from "@angular/core";
+import {
+  Directive,
+  ContentChildren,
+  QueryList,
+  AfterContentChecked
+} from "@angular/core";
+import { TodoComponent } from "../../todo/todo.component";
+import { nextTick } from "q";
 
 @Directive({
   selector: "[appTodoFocus]"
 })
-export class TodoFocusDirective {
+export class TodoFocusDirective implements AfterContentChecked {
+  @ContentChildren(TodoComponent) focusChildren: QueryList<TodoComponent>;
+  focusIndex: number = null;
+  shouldFocus = false;
   constructor() {}
+
+  ngAfterContentChecked() {
+    const todoList = this.focusChildren.toArray();
+    if (this.shouldFocus) {
+      nextTick(() => todoList[this.focusIndex + 1].focusText());
+      this.shouldFocus = false;
+    }
+    todoList.forEach((todo, index) => {
+      todo.enterChange.subscribe(() => {
+        this.focusIndex = index;
+        this.shouldFocus = true;
+      });
+    });
+  }
 }
