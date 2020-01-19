c32669ff5dbcbf4741645b69970ded6319e87063
tkdalic <himagami126@gmail.com>
2020-01-19 19:31:17 +0900

    build: yarn ng g component shared/layout

--- unikki/src/app/shared/shared.module.ts
+++ unikki/src/app/shared/shared.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { LayoutComponent } from "./layout/layout.component";
 
 @NgModule({
-  declarations: [],
+  declarations: [LayoutComponent],
   imports: [CommonModule]
 })
 export class SharedModule {}
