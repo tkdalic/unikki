be5cc13892dca192f8f5b4715ab9a5e5ffbd1bd8
tkdalic <himagami126@gmail.com>
2020-02-08 11:47:53 +0900

    add: gapi auth

--- unikki/src/app/service/gapi.service.ts
+++ unikki/src/app/service/gapi.service.ts
@@ -1,8 +1,35 @@
 import { Injectable } from "@angular/core";
+import { environment } from "./../..//environments/environment";
 
 @Injectable({
   providedIn: "root"
 })
 export class GapiService {
   constructor() {}
+
+  private static readonly PROJECT_ID = environment.projectId;
+  private static readonly CLIENT_ID = environment.clientId;
+  private static readonly API_KEY = environment.apiKey;
+  private static readonly SCOPES = environment.scopes;
+
+  /**
+   * Authorize Google Compute Engine API.
+   */
+  auth() {
+    gapi.client.setApiKey(GapiService.API_KEY);
+    gapi.auth.authorize(
+      {
+        client_id: GapiService.CLIENT_ID,
+        scope: GapiService.SCOPES,
+        immediate: false
+      },
+      function(authResult) {
+        if (authResult && !authResult.error) {
+          window.alert("Auth was successful!");
+        } else {
+          window.alert("Auth was not successful");
+        }
+      }
+    );
+  }
 }
--- unikki/src/environments/environment.prod.ts
+++ unikki/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: false,
+  projectId: "",
+  clientId: "",
+  apiKey: "",
+  scopes: ""
 };
