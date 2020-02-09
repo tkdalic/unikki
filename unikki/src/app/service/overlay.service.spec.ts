811730cbdaeaee3914c496b0bcef0f23edc5996f
tkdalic <himagami126@gmail.com>
2020-02-10 00:08:02 +0900

    modify: overlay service 状態管理

--- unikki/src/app/service/overlay.service.spec.ts
+++ unikki/src/app/service/overlay.service.spec.ts
@@ -9,4 +9,25 @@ describe("OverlayService", () => {
     const service: OverlayService = TestBed.get(OverlayService);
     expect(service).toBeTruthy();
   });
+
+  it("should show", () => {
+    const service: OverlayService = TestBed.get(OverlayService);
+    service.show();
+    expect(service.isDisplay).toBeTruthy();
+  });
+
+  it("should hide", () => {
+    const service: OverlayService = TestBed.get(OverlayService);
+    service.hide();
+    expect(service.isDisplay).toBeFalsy();
+  });
+
+  it("should toggle", () => {
+    const service: OverlayService = TestBed.get(OverlayService);
+    service.hide();
+    service.toggle();
+    expect(service.isDisplay).toBeTruthy();
+    service.toggle();
+    expect(service.isDisplay).toBeFalsy();
+  });
 });
--- unikki/src/app/service/overlay.service.ts
+++ unikki/src/app/service/overlay.service.ts
@@ -4,5 +4,21 @@ import { Injectable } from "@angular/core";
   providedIn: "root"
 })
 export class OverlayService {
+  private _isDisplay = false;
   constructor() {}
+
+  get isDisplay(): boolean {
+    return this._isDisplay;
+  }
+  show() {
+    this._isDisplay = true;
+  }
+
+  hide() {
+    this._isDisplay = false;
+  }
+
+  toggle() {
+    this._isDisplay = !this.isDisplay;
+  }
 }
