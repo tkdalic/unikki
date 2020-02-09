6cb96d65ac93abee5d79dd0cff9c2d6c7e341c2f
tkdalic <himagami126@gmail.com>
2020-02-09 11:01:07 +0900

    add: file service 実装

--- unikki/src/app/service/file.service.spec.ts
+++ unikki/src/app/service/file.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from "@angular/core/testing";
 
 import { FileService } from "./file.service";
+import { MarkdownFile } from "../resource-model/markdownFile";
 
 describe("FileService", () => {
   beforeEach(() => TestBed.configureTestingModule({}));
@@ -8,5 +9,19 @@ describe("FileService", () => {
   it("should be created", () => {
     const service: FileService = TestBed.get(FileService);
     expect(service).toBeTruthy();
+  });
+
+  it("should read and write file contents", async () => {
+    const service: FileService = TestBed.get(FileService);
+    const markdownFile: MarkdownFile = {
+      title: "test.md",
+      contents: "# test"
+    };
+    const file = service.make(markdownFile);
+    expect(file).toBeTruthy();
+    expect(file.type).toBe("text/markdown");
+    const contents = await service.read(file);
+    expect(contents.title).toBe(markdownFile.title);
+    expect(contents.contents).toBe(markdownFile.contents);
   });
 });
--- unikki/src/app/service/file.service.ts
+++ unikki/src/app/service/file.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from "@angular/core";
+import { MarkdownFile } from "../resource-model/markdownFile";
 
 @Injectable({
   providedIn: "root"
 })
 export class FileService {
-  constructor() {}
+  constructor() {
+    const file = this.make({ title: "hoge.md", contents: "# test" });
+    console.log(file);
+    this.read(file).then(result => console.log(result));
+  }
+
+  make(markdownFile: MarkdownFile): File {
+    return new File([markdownFile.contents], markdownFile.title, {
+      type: "text/markdown"
+    });
+  }
+
+  read(file: File): Promise<MarkdownFile> {
+    return new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.onload = () =>
+        resolve({ title: file.name, contents: fileReader.result as string });
+      fileReader.readAsText(file);
+    });
+  }
 }
