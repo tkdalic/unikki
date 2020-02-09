1a55e230b1b6723f267a9a092315923f83fa9fdd
tkdalic <himagami126@gmail.com>
2020-02-09 23:51:00 +0900

    build: yarn ng g component shared/overlay

--- unikki/src/app/shared/shared.module.ts
+++ unikki/src/app/shared/shared.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { LayoutComponent } from "./layout/layout.component";
 import { HeaderComponent } from "./header/header.component";
+import { OverlayComponent } from "./overlay/overlay.component";
 
 @NgModule({
-  declarations: [LayoutComponent, HeaderComponent],
+  declarations: [LayoutComponent, HeaderComponent, OverlayComponent],
   exports: [HeaderComponent, LayoutComponent],
   imports: [CommonModule]
 })
