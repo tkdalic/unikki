69e29d589d0599ec9c8bf6a585230dac22543e2e
tkdalic <himagami126@gmail.com>
2020-02-10 20:57:00 +0900

    rename: overlay to loading

--- unikki/src/app/service/loading.service.ts
+++ unikki/src/app/service/loading.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from "@angular/core";
 @Injectable({
   providedIn: "root"
 })
-export class OverlayService {
+export class LoadingService {
   private isDisplayFlg = false;
   constructor() {}
 
--- unikki/src/stories/overlay.stories.ts
+++
@@ -1,9 +0,0 @@
-import { OverlayComponent } from "src/app/shared/overlay/overlay.component";
-
-export default {
-  title: "overlay"
-};
-
-export const layout = () => ({
-  component: OverlayComponent
-});
