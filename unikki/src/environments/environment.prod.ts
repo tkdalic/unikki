211ad66680719ef366961005fcfea86c28311a9d
tkdalic <himagami126@gmail.com>
2020-02-10 09:13:59 +0900

    modify: environment prod keyなどの発行

--- unikki/src/environments/environment.prod.ts
+++ unikki/src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
-  production: false,
+  production: true,
   projectId: "",
-  clientId: "",
-  apiKey: "",
-  scopes: ""
+  clientId:
+    "290772003297-l7kes990q71r06cc8nvvfcid8bkddqbf.apps.googleusercontent.com",
+  apiKey: "AIzaSyDOw2WgmnBEdPNVhKbckmtH9ZqoCw-7-II",
+  scopes: ["https://www.googleapis.com/auth/drive.file"].join(" ")
 };
