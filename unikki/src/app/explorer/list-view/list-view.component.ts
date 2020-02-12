1abda26ec9b5f953fef67316d07bdaeb505966f1
tkdalic <himagami126@gmail.com>
2020-02-12 09:36:58 +0900

    modify: list-view 受け取った要素を描画するように

--- unikki/src/app/explorer/list-view/list-view.component.ts
+++ unikki/src/app/explorer/list-view/list-view.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
+import { FileItem } from "src/app/resource-model/fileItem";
 
 @Component({
   selector: "app-list-view",
@@ -6,7 +7,18 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./list-view.component.scss"]
 })
 export class ListViewComponent implements OnInit {
+  @Input() items: Array<FileItem> = [
+    { name: "name", id: "id" },
+    { name: "name", id: "id" },
+    { name: "name", id: "id" },
+    { name: "name", id: "id" },
+    { name: "name", id: "id" }
+  ];
   constructor() {}
 
   ngOnInit(): void {}
+
+  trackItem(index: number, item: FileItem): string {
+    return item.id;
+  }
 }
