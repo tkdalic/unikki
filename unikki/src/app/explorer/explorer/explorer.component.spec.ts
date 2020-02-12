a44ec54eb236818a97329720a987b9f98647bd35
tkdalic <himagami126@gmail.com>
2020-02-12 23:39:44 +0900

    modify: explorer spec declarationsにlistviewを追加

--- unikki/src/app/explorer/explorer/explorer.component.spec.ts
+++ unikki/src/app/explorer/explorer/explorer.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { ExplorerComponent } from "./explorer.component";
+import { ListViewComponent } from "../list-view/list-view.component";
 
 describe("ExplorerComponent", () => {
   let component: ExplorerComponent;
@@ -8,7 +9,7 @@ describe("ExplorerComponent", () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ExplorerComponent]
+      declarations: [ExplorerComponent, ListViewComponent]
     }).compileComponents();
   }));
 
