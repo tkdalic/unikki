9ecddd364a4db7ca8a7a9d1dec3afba783a6e815
tkdalic <himagami126@gmail.com>
2020-02-12 09:05:57 +0900

    build: yarn ng g module explorer --routing

---
+++ unikki/src/app/explorer/explorer.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+import { ExplorerRoutingModule } from "./explorer-routing.module";
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, ExplorerRoutingModule]
+})
+export class ExplorerModule {}
