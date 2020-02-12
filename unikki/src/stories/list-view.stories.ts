8dafb877f40959b2fc71f8102db222e6c29e7ee5
tkdalic <himagami126@gmail.com>
2020-02-12 09:10:05 +0900

    add: list-view storybook

---
+++ unikki/src/stories/list-view.stories.ts
@@ -0,0 +1,10 @@
+import { ListViewComponent } from "src/app/explorer/list-view/list-view.component";
+
+export default {
+  title: "list-view"
+};
+
+export const todo = () => ({
+  component: ListViewComponent,
+  props: {}
+});
