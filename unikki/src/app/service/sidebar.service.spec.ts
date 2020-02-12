1b3687142dd11352ba993f8439912004f9c7886a
tkdalic <himagami126@gmail.com>
2020-02-12 23:08:28 +0900

    modify: sidebar display周りの関数を追加

--- unikki/src/app/service/sidebar.service.spec.ts
+++ unikki/src/app/service/sidebar.service.spec.ts
@@ -13,4 +13,25 @@ describe("SidebarService", () => {
   it("should be created", () => {
     expect(service).toBeTruthy();
   });
+
+  it("should show", () => {
+    const service: SidebarService = TestBed.inject(SidebarService);
+    service.show();
+    expect(service.isDisplay).toBeTruthy();
+  });
+
+  it("should hide", () => {
+    const service: SidebarService = TestBed.inject(SidebarService);
+    service.hide();
+    expect(service.isDisplay).toBeFalsy();
+  });
+
+  it("should toggle", () => {
+    const service: SidebarService = TestBed.inject(SidebarService);
+    service.hide();
+    service.toggle();
+    expect(service.isDisplay).toBeTruthy();
+    service.toggle();
+    expect(service.isDisplay).toBeFalsy();
+  });
 });
--- unikki/src/app/service/sidebar.service.ts
+++ unikki/src/app/service/sidebar.service.ts
@@ -4,5 +4,21 @@ import { Injectable } from "@angular/core";
   providedIn: "root"
 })
 export class SidebarService {
+  isDisplayFlg: boolean;
   constructor() {}
+
+  get isDisplay(): boolean {
+    return this.isDisplayFlg;
+  }
+  show() {
+    this.isDisplayFlg = true;
+  }
+
+  hide() {
+    this.isDisplayFlg = false;
+  }
+
+  toggle() {
+    this.isDisplayFlg = !this.isDisplay;
+  }
 }
