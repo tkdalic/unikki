cd9072d668ea8f3fafbfa179f1e49642e3f6079c
tkdalic <himagami126@gmail.com>
2020-02-15 12:04:39 +0900

    modify: service worker spec 依存の解決

--- unikki/src/app/app.component.spec.ts
+++ unikki/src/app/app.component.spec.ts
@@ -2,11 +2,19 @@ import { TestBed, async } from "@angular/core/testing";
 import { RouterTestingModule } from "@angular/router/testing";
 import { AppComponent } from "./app.component";
 import { SharedModule } from "./shared/shared.module";
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { environment } from "src/environments/environment";
 
 describe("AppComponent", () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule, SharedModule],
+      imports: [
+        RouterTestingModule,
+        SharedModule,
+        ServiceWorkerModule.register("ngsw-worker.js", {
+          enabled: environment.production
+        })
+      ],
       declarations: [AppComponent]
     }).compileComponents();
   }));
--- unikki/src/app/service/sw-update.service.spec.ts
+++ unikki/src/app/service/sw-update.service.spec.ts
@@ -1,12 +1,20 @@
 import { TestBed } from "@angular/core/testing";
 
 import { SwUpdateService } from "./sw-update.service";
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { environment } from "src/environments/environment";
 
 describe("SwUpdateService", () => {
   let service: SwUpdateService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        ServiceWorkerModule.register("ngsw-worker.js", {
+          enabled: environment.production
+        })
+      ]
+    });
     service = TestBed.inject(SwUpdateService);
   });
 
