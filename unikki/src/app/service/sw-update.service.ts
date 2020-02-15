b0bb7bb2a1e054c0229049a5311c29b8d7eda33f
tkdalic <himagami126@gmail.com>
2020-02-15 11:45:09 +0900

    modify: sw-update 設定の追記

--- unikki/src/app/service/sw-update.service.ts
+++ unikki/src/app/service/sw-update.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from "@angular/core";
+import { SwUpdate } from "@angular/service-worker";
 
 @Injectable({
   providedIn: "root"
 })
 export class SwUpdateService {
-  constructor() {}
+  constructor(updates: SwUpdate) {
+    updates.available.subscribe(event => {
+      updates.activateUpdate().then(() => document.location.reload());
+    });
+  }
 }
