d7556877f342907bc43fc6fc96d789af636823f3
tkdalic <himagami126@gmail.com>
2020-02-16 13:14:15 +0900

    modify: loading materialに変更

--- unikki/src/app/shared/shared.module.ts
+++ unikki/src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from "@angular/common";
 import { LayoutComponent } from "./layout/layout.component";
 import { HeaderComponent } from "./header/header.component";
 import { LoadingComponent } from "./loading/loading.component";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 
 @NgModule({
   declarations: [LayoutComponent, HeaderComponent, LoadingComponent],
   exports: [HeaderComponent, LayoutComponent, LoadingComponent],
-  imports: [CommonModule]
+  imports: [CommonModule, MatProgressSpinnerModule]
 })
 export class SharedModule {}
