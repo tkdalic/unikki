7183bb269445af1cf8c6f5b6105aecf141b542c6
tkdalic <himagami126@gmail.com>
2020-02-16 19:33:01 +0900

    modify: list view materialize

--- unikki/src/stories/list-view.stories.ts
+++ unikki/src/stories/list-view.stories.ts
@@ -1,16 +1,24 @@
 import { ListViewComponent } from "src/app/explorer/list-view/list-view.component";
+import { moduleMetadata } from "@storybook/angular";
+import { MatListModule } from "@angular/material/list";
 
 export default {
-  title: "list-view"
+  title: "list-view",
+  decorators: [
+    moduleMetadata({
+      imports: [MatListModule]
+    })
+  ]
 };
 
 export const todo = () => ({
   component: ListViewComponent,
   props: {
+    selectId: "hoge1",
     items: [
-      { name: "hoge1", id: "hoge" },
-      { name: "hoge2", id: "hoge" },
-      { name: "hoge3", id: "hoge" }
+      { name: "hoge1", id: "hoge1" },
+      { name: "hoge2", id: "hoge2" },
+      { name: "hoge3", id: "hoge3" }
     ]
   }
 });
