6f5a4f8ca6c819916c37faed4ef9e27652f7bcad
tkdalic <himagami126@gmail.com>
2020-02-16 15:28:05 +0900

    modify: list-view stories propsの追加

--- unikki/src/stories/list-view.stories.ts
+++ unikki/src/stories/list-view.stories.ts
@@ -6,5 +6,11 @@ export default {
 
 export const todo = () => ({
   component: ListViewComponent,
-  props: {}
+  props: {
+    items: [
+      { name: "hoge1", id: "hoge" },
+      { name: "hoge2", id: "hoge" },
+      { name: "hoge3", id: "hoge" }
+    ]
+  }
 });
